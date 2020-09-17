3eacfe8258c0c60b9905694ffad81aa0e99ebc91
knsnedden <kayleah.snedden@trojans.dsu.edu>
2020-09-17 15:44:22 -0700

    mergeSort implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,9 +14,17 @@ public class Main {
         }
         System.out.println();
 
-	    selectionSort(output,10,10); // testing selection sort
+	    char[][] ssArr = output, msArr = output;
+	    for (int i = 0; i < 10; ++i) {
+            selectionSort(ssArr[i],10); // testing selection sort
+            String str = Arrays.toString(ssArr[i])+" ";
+            System.out.print(str);
+        }
+
+        System.out.println();
         for (int i = 0; i < 10; ++i){
-            String str = Arrays.toString(output[i])+" ";
+            mergeSort(ssArr[i],0,9);
+            String str = Arrays.toString(msArr[i])+" ";
             System.out.print(str);
         }
     }
@@ -39,49 +47,83 @@ public class Main {
     }
 
     //Simple sort: Selection Sort
-    public static void selectionSort(char[][] arr, int N, int k){
+    public static void selectionSort(char[] arr, int k){
         int min;
 
-        // THIS IS IS WE ARE SUPPOSED TO SORT THE STRINGS THEMSELVES
-        // loop through entire array to sort
-        for (int i = 0; i < N; ++i){
-            // loop through each string and sort the characters
-            for (int j = 0; j < k-1; ++j){
-                min = j; // set the index we are trying to sort to the minimum
-                // look for the minimum char in the string
-                for (int x = j+1; x < k; ++x){
-                    if (arr[i][x] < arr[i][min]){
-                        min = x; // if a smaller char is found, store the index
-                    }
-                }
 
-                // once the smallest element has been found, move it to index j
-                swap(arr, min, j, i);
+        // loop through string and sort the characters
+        for (int i = 0; i < k-1; ++i) {
+            min = i; // set the index we are trying to sort to the minimum
+            // look for the minimum char in the string
+            for (int j = i + 1; j < k; ++j) {
+                if (arr[j] < arr[min]) {
+                    min = j; // if a smaller char is found, store the index
+                }
             }
+
+            // once the smallest element has been found, move it to index j
+            swap(arr, min, i);
         }
-        // THIS IS IF WE ARE COMPARING STRINGS TO ONE ANOTHER -- unclear if this is what we need or if it's each string by element
-      /*  for (int i = 0; i < N-1; ++i){
-            min = i;
-            for (int j = i+1; j < N; ++j){
-                String minStr = Arrays.toString(arr[min]);
-                String comp = Arrays.toString(arr[j]);
-                if (comp.compareTo(minStr) < 0){
-                    min = j;
-                }
-            }
-            swapTwo(arr,min,i);
-        }*/
     }
 
-    public static void swap(char[][] arr, int a, int b, int i){
-        char temp = arr[i][a];
-        arr[i][a] = arr[i][b];
-        arr[i][b] = temp;
+    public static void mergeSort(char[] arr, int left, int right){
+        if (left < right){
+            int middle = left + (right - left)/2; // locate the middle index of current list
+
+            mergeSort(arr,left,middle); // merge sorts the left half of current list
+            mergeSort(arr,middle+1,right); // merge sorts right half of current list
+
+            //merging lists back together
+            merge(arr,left, middle, right);
+
+        }
     }
-    /*public static void swapTwo(char[][] arr, int a, int b){
-        char[] temp = arr[a];
+    public static void merge(char[] arr, int left, int middle, int right){
+        // determine the sizes of the left and right halves of array section
+        int leftSize = middle - left + 1;
+        int rightSize = right - middle;
+        // create new arrays to hold the left and right halves of array section
+        char[] leftArr = new char[leftSize];
+        char[] rightArr = new char[rightSize];
+
+        // populate the arrays
+        for (int i = 0; i < leftSize; ++i){
+            leftArr[i] = arr[left + i];
+        }
+        for (int j = 0; j < rightSize; ++j){
+            rightArr[j] = arr[middle + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        // loop through the left and right arrays to sort this section of the array
+        while (i < leftSize && j < rightSize){
+            if (leftArr[i] <= rightArr[j]){ // if the current left array element is smaller than right array element, place in smallest available spot in overall array
+                arr[k] = leftArr[i];
+                i++;
+            }
+            else{ // if not, place right array element into smallest available spot in overall array
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < leftSize){ // if any leftover elements in left array, put them in overall array
+            arr[k] = leftArr[i];
+            ++i;
+            ++k;
+        }
+        while (j < rightSize){ // if any leftover elements in right array, put them in overall array
+            arr[k] = rightArr[j];
+            ++j;
+            ++k;
+        }
+    }
+
+
+    public static void swap(char[] arr, int a, int b){
+        char temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
-    }*/
+    }
 
 }
