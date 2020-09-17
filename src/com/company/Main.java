6829c727aa9360a8a9b52a4e5c96c953a2e0b68a
knsnedden <kayleah.snedden@trojans.dsu.edu>
2020-09-17 14:07:37 -0700

    generateTestList implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    char[][] output;
+	    output = generateTestList(10,10,65,90); // testing generation
+	    for (int i = 0; i < 10; ++i){
+	        System.out.println(output[i]);
+        }
+    }
+
+    public static char[][] generateTestList(int N, int k, int minV, int maxV){
+        char[][] arr = new char[N][k+1];
+        Random randChar = new Random();
+        char insert;
+
+        // fill array with strings
+        for (int i = 0; i < N; ++i){
+            for (int j = 0; j < k; ++j){
+                insert = (char)(randChar.nextInt(maxV-minV) + minV); // generate random character
+                arr[i][j] = insert;
+            }
+            arr[i][k] = 0; // end string with null byte
+        }
+
+        return arr;
     }
 }
